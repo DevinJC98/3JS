b0bfa45796acb32e585e57be26e4c9ac3b8a0231
DevinJC98 <devinjc98@hotmail.com>
2025-04-11 01:17:36 -0600

    Camera & Star Generation Updates

--- main.js
+++ main.js
@@ -40,18 +40,26 @@ scene.add(pointlight, ambience);
 function animate() {
   renderer.render(scene, camera);
 
+  mercury.rotation.y += 0.005;
+  venus.rotation.y += 0.005;
   earth.rotation.y += 0.005;
-  earthmoon.rotation.y += 0.0005;
-
+  earthmoon.rotation.y += 0.002;
+  mars.rotation.y += 0.005;
   jupiter.rotation.y += 0.005;
+  saturn.rotation.y += 0.005;
   ringholder.rotation.y += 0.01;
+  uranus.rotation.y += 0.005;
+  neptune.rotation.y += 0.005;
 }
 
 //runs the animation in the renderer
 renderer.setAnimationLoop(animate);
 
 //camera controls for click and drag
+//Default Target is 0,0,0
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
+controls.enableZoom = false;
 
 //random generation
 function addstar() {
@@ -60,10 +68,15 @@ function addstar() {
   const material = new THREE.MeshBasicMaterial(0xffffff);
   const sphere = new THREE.Mesh(geometry, material);
 
+  const yzmin = -750;
+  const yzmax = 750;
+  const xmin = -1800;
+  const xmax = 1800;
+
   //build an array of three, fill it with random values
-  const [x, y, z] = Array(3)
-    .fill() // this function is built in 3js function that returns a random positive int within the specified range
-    .map(() => THREE.MathUtils.randInt(-1300, 1000));
+  const y = Math.random() * (yzmax - yzmin) + yzmin;
+  const z = Math.random() * (yzmax - yzmin) + yzmin;
+  const x = Math.random() * (xmax - xmin) + xmin;
 
   //set star coords with previously built array ^
   sphere.position.set(x, y, z);
@@ -80,9 +93,11 @@ const planet = new THREE.SphereGeometry(25);
 //sun
 const sunmaterial = new THREE.MeshStandardMaterial(0xf5f749);
 sunmaterial.emissive = new THREE.Color(0xfdfdb3);
+sunmaterial.emissive.intensity = 2;
 const sun = new THREE.Mesh(planet, sunmaterial);
 sun.scale.setScalar(15);
 sun.position.set(-1200, 0, 0);
+sun.name = "Sun";
 scene.add(sun);
 
 //mercury
@@ -92,6 +107,7 @@ const mercurymaterial = new THREE.MeshStandardMaterial({ map: mercurytexture });
 const mercury = new THREE.Mesh(planet, mercurymaterial);
 mercury.position.x = -600;
 mercury.scale.setScalar(0.3);
+mercury.name = "Mercury";
 scene.add(mercury);
 
 //venus
@@ -100,6 +116,7 @@ const venusmaterial = new THREE.MeshStandardMaterial({ map: venustexture });
 const venus = new THREE.Mesh(planet, venusmaterial);
 venus.position.x = -300;
 venus.scale.setScalar(0.9);
+venus.name = "Venus";
 scene.add(venus);
 
 //earth
@@ -117,6 +134,7 @@ const moonmaterial = new THREE.MeshStandardMaterial({ map: moontexture });
 const moon = new THREE.Mesh(planet, moonmaterial);
 moon.position.set(50, 0, 0);
 moon.scale.setScalar(0.25);
+moon.name = "Moon";
 const earthmoon = new THREE.Group();
 earthmoon.add(moon);
 scene.add(earthmoon);
@@ -128,6 +146,7 @@ const marsmaterial = new THREE.MeshStandardMaterial({ map: marstexture });
 const mars = new THREE.Mesh(planet, marsmaterial);
 mars.position.x = 300;
 mars.scale.setScalar(0.5);
+mars.name = "Mars";
 scene.add(mars);
 
 //jupiter
@@ -136,40 +155,44 @@ const jupitermaterial = new THREE.MeshStandardMaterial({ map: jupitertexture });
 const jupiter = new THREE.Mesh(planet, jupitermaterial);
 jupiter.position.x = 600;
 jupiter.scale.setScalar(4);
+jupiter.name = "Jupiter";
 scene.add(jupiter);
 
 //saturn
 //make shape in blender and import instead of using build in shapes
 const ringshape = new THREE.TorusGeometry(33, 5, 2, 50);
-
 const saturntexture = new THREE.TextureLoader().load("Saturn.webp");
 const saturnmaterial = new THREE.MeshStandardMaterial({ map: saturntexture });
 const saturn = new THREE.Mesh(planet, saturnmaterial);
 const saturnsring = new THREE.Mesh(ringshape, jupitermaterial);
 saturnsring.rotation.x = 90;
 saturnsring.rotation.z = 20;
-saturn.position.x = 900;
+saturn.position.x = 1100;
 saturn.scale.setScalar(3);
 saturnsring.scale.setScalar(3);
 const ringholder = new THREE.Group();
-ringholder.position.x = 900;
+ringholder.position.x = 1100;
 ringholder.add(saturnsring);
+saturn.name = "Saturn";
+saturnsring.name = "Saturn";
 scene.add(saturn, ringholder);
 
 //uranus
 const uranustexture = new THREE.TextureLoader().load("uranus.webp");
 const uranusmaterial = new THREE.MeshStandardMaterial({ map: uranustexture });
 const uranus = new THREE.Mesh(planet, uranusmaterial);
-uranus.position.x = 1200;
+uranus.position.x = 1500;
 uranus.scale.setScalar(2);
+uranus.name = "Uranus";
 scene.add(uranus);
 
 //neptune
 const neptunetexture = new THREE.TextureLoader().load("neptune.webp");
 const neptunematerial = new THREE.MeshStandardMaterial({ map: neptunetexture });
 const neptune = new THREE.Mesh(planet, neptunematerial);
-neptune.position.x = 1500;
+neptune.position.x = 1900;
 neptune.scale.setScalar(2);
+neptune.name = "Neptune";
 scene.add(neptune);
 
 //UI Events
@@ -185,15 +208,15 @@ pastbutton.addEventListener("click", function () {
   switch (planetindex) {
     case 0:
       const sunmove = setInterval(function () {
-        camera.position.x -= warpspeed;
-        camera.position.z += 1.2;
-        if (camera.position.x <= sun.position.x) {
-          controls.target = new THREE.Vector3(
-            sun.position.x,
-            sun.position.y,
-            sun.position.z
-          );
-          controls.update();
+        camera.position.x -= warpspeed * 1.6;
+        if (camera.position.z >= 0) {
+          camera.position.z += 2;
+        } else {
+          camera.position.z -= 2;
+        }
+        controls.target.x -= warpspeed * 1.6;
+        controls.update();
+        if (controls.target.x <= sun.position.x) {
           clearInterval(sunmove);
         }
       }, 2);
@@ -202,14 +225,16 @@ pastbutton.addEventListener("click", function () {
     case 1:
       const mercurymove = setInterval(function () {
         camera.position.x -= warpspeed;
-        camera.position.z -= 0.3;
-        if (camera.position.x <= mercury.position.x) {
-          controls.target = new THREE.Vector3(
-            mercury.position.x,
-            mercury.position.y,
-            mercury.position.z
-          );
-          controls.update();
+
+        if (camera.position.z >= 0) {
+          camera.position.z -= 0.12;
+        } else {
+          camera.position.z += 0.12;
+        }
+
+        controls.target.x -= warpspeed;
+        controls.update();
+        if (controls.target.x <= mercury.position.x) {
           clearInterval(mercurymove);
         }
       }, 2);
@@ -218,13 +243,14 @@ pastbutton.addEventListener("click", function () {
     case 2:
       const venusmove = setInterval(function () {
         camera.position.x -= warpspeed;
-        if (camera.position.x <= venus.position.x) {
-          controls.target = new THREE.Vector3(
-            venus.position.x,
-            venus.position.y,
-            venus.position.z
-          );
-          controls.update();
+        if (camera.position.z >= 0) {
+          camera.position.z -= 0.1;
+        } else {
+          camera.position.z += 0.1;
+        }
+        controls.target.x -= warpspeed;
+        controls.update();
+        if (controls.target.x <= venus.position.x) {
           clearInterval(venusmove);
         }
       }, 2);
@@ -233,13 +259,14 @@ pastbutton.addEventListener("click", function () {
     case 3:
       const earthmove = setInterval(function () {
         camera.position.x -= warpspeed;
-        if (camera.position.x <= earth.position.x) {
-          controls.target = new THREE.Vector3(
-            earth.position.x,
-            earth.position.y,
-            earth.position.z
-          );
-          controls.update();
+        if (camera.position.z >= 0) {
+          camera.position.z += 0.2;
+        } else {
+          camera.position.z -= 0.2;
+        }
+        controls.target.x -= warpspeed;
+        controls.update();
+        if (controls.target.x <= earth.position.x) {
           clearInterval(earthmove);
         }
       }, 2);
@@ -248,13 +275,14 @@ pastbutton.addEventListener("click", function () {
     case 4:
       const marsmove = setInterval(function () {
         camera.position.x -= warpspeed;
-        if (camera.position.x <= mars.position.x) {
-          controls.target = new THREE.Vector3(
-            mars.position.x,
-            mars.position.y,
-            mars.position.z
-          );
-          controls.update();
+        if (camera.position.z >= 0) {
+          camera.position.z -= 0.5;
+        } else {
+          camera.position.z += 0.5;
+        }
+        controls.target.x -= warpspeed;
+        controls.update();
+        if (controls.target.x <= mars.position.x) {
           clearInterval(marsmove);
         }
       }, 2);
@@ -263,13 +291,9 @@ pastbutton.addEventListener("click", function () {
     case 5:
       const jupitermove = setInterval(function () {
         camera.position.x -= warpspeed;
-        if (camera.position.x <= jupiter.position.x) {
-          controls.target = new THREE.Vector3(
-            jupiter.position.x,
-            jupiter.position.y,
-            jupiter.position.z
-          );
-          controls.update();
+        controls.target.x -= warpspeed;
+        controls.update();
+        if (controls.target.x <= jupiter.position.x) {
           clearInterval(jupitermove);
         }
       }, 2);
@@ -277,14 +301,15 @@ pastbutton.addEventListener("click", function () {
 
     case 6:
       const saturnmove = setInterval(function () {
-        camera.position.x -= warpspeed;
-        if (camera.position.x <= saturn.position.x) {
-          controls.target = new THREE.Vector3(
-            saturn.position.x,
-            saturn.position.y,
-            saturn.position.z
-          );
-          controls.update();
+        camera.position.x -= warpspeed * 1.6;
+        if (camera.position.z >= 0) {
+          camera.position.z += 0.2;
+        } else {
+          camera.position.z -= 0.2;
+        }
+        controls.target.x -= warpspeed * 1.6;
+        controls.update();
+        if (controls.target.x <= saturn.position.x) {
           clearInterval(saturnmove);
         }
       }, 2);
@@ -292,35 +317,17 @@ pastbutton.addEventListener("click", function () {
 
     case 7:
       const uranusmove = setInterval(function () {
-        camera.position.x -= warpspeed;
-        if (camera.position.x <= uranus.position.x) {
-          controls.target = new THREE.Vector3(
-            uranus.position.x,
-            uranus.position.y,
-            uranus.position.z
-          );
-          controls.update();
+        camera.position.x -= warpspeed * 1.3;
+        controls.target.x -= warpspeed * 1.3;
+        controls.update();
+        if (controls.target.x <= uranus.position.x) {
           clearInterval(uranusmove);
-        }
-      }, 2);
-      break;
-
-    case 8:
-      const neptunemove = setInterval(function () {
-        camera.position.x -= warpspeed;
-        if (camera.position.x <= neptune.position.x) {
-          controls.target = new THREE.Vector3(
-            neptune.position.x,
-            neptune.position.y,
-            neptune.position.z
-          );
-          controls.update();
-          clearInterval(neptunemove);
         }
       }, 2);
       break;
   }
 });
+
 nextbutton.addEventListener("click", function () {
   planetindex++;
   if (planetindex >= 8) {
@@ -329,121 +336,119 @@ nextbutton.addEventListener("click", function () {
 
   switch (planetindex) {
     case 1:
-      const mercurymove = setInterval(function () {
-        camera.position.x += warpspeed;
-        if (camera.position.x >= mercury.position.x) {
-          controls.target = new THREE.Vector3(
-            mercury.position.x,
-            mercury.position.y,
-            mercury.position.z
-          );
-          controls.update();
-          clearInterval(mercurymove);
+      const fromsun = setInterval(function () {
+        camera.position.x += warpspeed * 1.6;
+        if (camera.position.z >= 0) {
+          camera.position.z -= 2;
+        } else {
+          camera.position.z += 2;
+        }
+        controls.target.x += warpspeed * 1.6;
+        controls.update();
+        if (controls.target.x >= mercury.position.x) {
+          clearInterval(fromsun);
         }
       }, 2);
       break;
 
     case 2:
-      const venusmove = setInterval(function () {
+      const frommercury = setInterval(function () {
         camera.position.x += warpspeed;
-        if (camera.position.x >= venus.position.x) {
-          controls.target = new THREE.Vector3(
-            venus.position.x,
-            venus.position.y,
-            venus.position.z
-          );
-          controls.update();
-          clearInterval(venusmove);
+        if (camera.position.z >= 0) {
+          camera.position.z += 0.12;
+        } else {
+          camera.position.z -= 0.12;
+        }
+        controls.target.x += warpspeed;
+        controls.update();
+        if (controls.target.x >= venus.position.x) {
+          clearInterval(frommercury);
         }
       }, 2);
       break;
 
     case 3:
-      const earthmove = setInterval(function () {
+      const fromvenus = setInterval(function () {
         camera.position.x += warpspeed;
-        if (camera.position.x >= earth.position.x) {
-          controls.target = new THREE.Vector3(
-            earth.position.x,
-            earth.position.y,
-            earth.position.z
-          );
-          controls.update();
-          clearInterval(earthmove);
+        if (camera.position.z >= 0) {
+          camera.position.z += 0.1;
+        } else {
+          camera.position.z -= 0.1;
+        }
+        controls.target.x += warpspeed;
+        controls.update();
+        if (controls.target.x >= earth.position.x) {
+          clearInterval(fromvenus);
         }
       }, 2);
       break;
 
     case 4:
-      const marsmove = setInterval(function () {
+      const fromearth = setInterval(function () {
         camera.position.x += warpspeed;
-        if (camera.position.x >= mars.position.x) {
-          controls.target = new THREE.Vector3(
-            mars.position.x,
-            mars.position.y,
-            mars.position.z
-          );
-          controls.update();
-          clearInterval(marsmove);
+        if (camera.position.z >= 0) {
+          camera.position.z -= 0.2;
+        } else {
+          camera.position.z += 0.2;
+        }
+        controls.target.x += warpspeed;
+        controls.update();
+        if (controls.target.x >= mars.position.x) {
+          clearInterval(fromearth);
         }
       }, 2);
       break;
 
     case 5:
-      const jupitermove = setInterval(function () {
+      const frommars = setInterval(function () {
         camera.position.x += warpspeed;
-        if (camera.position.x >= jupiter.position.x) {
-          controls.target = new THREE.Vector3(
-            jupiter.position.x,
-            jupiter.position.y,
-            jupiter.position.z
-          );
-          controls.update();
-          clearInterval(jupitermove);
+        if (camera.position.z >= 0) {
+          camera.position.z += 0.5;
+        } else {
+          camera.position.z -= 0.5;
+        }
+        controls.target.x += warpspeed;
+        controls.update();
+        if (controls.target.x >= jupiter.position.x) {
+          clearInterval(frommars);
         }
       }, 2);
       break;
 
     case 6:
-      const saturnmove = setInterval(function () {
-        camera.position.x += warpspeed;
-        if (camera.position.x >= saturn.position.x) {
-          controls.target = new THREE.Vector3(
-            saturn.position.x,
-            saturn.position.y,
-            saturn.position.z
-          );
-          controls.update();
-          clearInterval(saturnmove);
+      const fromjupiter = setInterval(function () {
+        camera.position.x += warpspeed * 1.6;
+        controls.target.x += warpspeed * 1.6;
+        controls.update();
+        if (controls.target.x >= saturn.position.x) {
+          clearInterval(fromjupiter);
         }
       }, 2);
       break;
 
     case 7:
-      const uranusmove = setInterval(function () {
-        camera.position.x += warpspeed;
-        if (camera.position.x >= uranus.position.x) {
-          controls.target = new THREE.Vector3(
-            uranus.position.x,
-            uranus.position.y,
-            uranus.position.z
-          );
-          controls.update();
-          clearInterval(uranusmove);
+      const fromsaturn = setInterval(function () {
+        camera.position.x += warpspeed * 1.3;
+        if (camera.position.z >= 0) {
+          camera.position.z -= 0.2;
+        } else {
+          camera.position.z += 0.2;
+        }
+        controls.target.x += warpspeed * 1.3;
+        controls.update();
+        if (controls.target.x >= uranus.position.x) {
+          clearInterval(fromsaturn);
         }
       }, 2);
       break;
 
     case 8:
-      const neptunemove = setInterval(function () {
-        camera.position.x += warpspeed;
-        if (camera.position.x >= neptune.position.x) {
-          controls.target = new THREE.Vector3(
-            neptune.position.x,
-            neptune.position.y,
-            neptune.position.z
-          );
-          controls.update();
-          clearInterval(neptunemove);
+      const fromuranus = setInterval(function () {
+        camera.position.x += warpspeed * 1.3;
+        controls.target.x += warpspeed * 1.3;
+        controls.update();
+        if (controls.target.x >= neptune.position.x) {
+          clearInterval(fromuranus);
         }
       }, 2);
       break;
@@ -471,7 +476,7 @@ window.addEventListener("mousemove", function (mousestuff) {
   }
   //if the ray isnt touching anything, display planet not found message and display default cursor
   if (intersects.length == 0) {
-    planetname.innerHTML = "Planet Not Found";
+    planetname.innerHTML = "Scanning";
     this.document.body.style.cursor = "initial";
   }
 });
